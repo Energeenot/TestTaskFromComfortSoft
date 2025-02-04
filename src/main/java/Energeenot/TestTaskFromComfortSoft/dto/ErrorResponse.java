1975645ca69694c4f23a01eed5855516ff8676ae
Energeenot <abramov.pandos@yandex.ru>
2025-02-05 01:43:21 +0300

    Добавил описание поля

--- src/main/java/Energeenot/TestTaskFromComfortSoft/dto/ErrorResponse.java
+++ src/main/java/Energeenot/TestTaskFromComfortSoft/dto/ErrorResponse.java
@@ -1,7 +1,13 @@
 package Energeenot.TestTaskFromComfortSoft.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 public class ErrorResponse {
 
+    @Schema(description = "Описание ошибки", example = """
+            {
+            "error": "Файл не найден: java.io.FileNotFoundException: C:/data/numbers.xlsx (Не удается найти указанный файл)"
+            }""")
     private String error;
 
     public ErrorResponse(String error) {
