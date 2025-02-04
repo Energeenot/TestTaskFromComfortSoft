d5de23bd3608c0a0e62d476ab8418f8da46ff641
Energeenot <abramov.pandos@yandex.ru>
2025-02-05 01:22:56 +0300

    привёл все ошибки к единому json формату

--- src/main/java/Energeenot/TestTaskFromComfortSoft/exception/SearcherExceptionHandler.java
+++ src/main/java/Energeenot/TestTaskFromComfortSoft/exception/SearcherExceptionHandler.java
@@ -1,5 +1,6 @@
 package Energeenot.TestTaskFromComfortSoft.exception;
 
+import Energeenot.TestTaskFromComfortSoft.dto.ErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
@@ -16,17 +17,17 @@ import java.util.Map;
 public class SearcherExceptionHandler {
 
     @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity<String> illegalArgumentExceptionHandler(IllegalArgumentException e) {
-        return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(e.getMessage());
+    public ResponseEntity<ErrorResponse> illegalArgumentExceptionHandler(IllegalArgumentException e) {
+        return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(new ErrorResponse(e.getMessage()));
     }
 
     @ExceptionHandler(RuntimeException.class)
-    public ResponseEntity<String> runtimeExceptionHandler(RuntimeException e) {
+    public ResponseEntity<ErrorResponse> runtimeExceptionHandler(RuntimeException e) {
         if (e.getCause() instanceof FileNotFoundException) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Файл не найден: " + e.getMessage());
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorResponse("Файл не найден: " + e.getMessage()));
         } else if (e.getCause() instanceof IOException) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Ошибка при обработке файла: " + e.getMessage());
-        } else return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Произошла ошибка сервера: " + e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ErrorResponse("Ошибка при обработке файла: " + e.getMessage()));
+        } else return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ErrorResponse("Произошла ошибка сервера: " + e.getMessage()));
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
