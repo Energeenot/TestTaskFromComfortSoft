bf568b422c1e7086d4e05765d83770e016754dfc
Energeenot <abramov.pandos@yandex.ru>
2025-02-05 00:34:34 +0300

    Initial commit

---
+++ src/main/java/Energeenot/TestTaskFromComfortSoft/TestTaskFromComfortSoftApplication.java
@@ -0,0 +1,13 @@
+package Energeenot.TestTaskFromComfortSoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestTaskFromComfortSoftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestTaskFromComfortSoftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Energeenot/TestTaskFromComfortSoft/TestTaskFromComfortSoftApplicationTests.java
@@ -0,0 +1,13 @@
+package Energeenot.TestTaskFromComfortSoft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestTaskFromComfortSoftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
